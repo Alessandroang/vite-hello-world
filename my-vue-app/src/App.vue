9ab5452e30ad68dd79a61a6bc9cea40954c5120f
Alessandroang <alessandroang94@gmail.com>
2023-09-08 09:11:25 +0200

    Add title and image

---
+++ my-vue-app/src/App.vue
@@ -0,0 +1,18 @@
+<script>
+import AppHero from "./components/AppHero.vue";
+export default {
+  data() {
+    return {
+      title: "La mia prima app con vite",
+    };
+  },
+  components: { AppHero },
+};
+</script>
+
+<template>
+  <AppHero />
+  <h1>{{ title }}</h1>
+</template>
+
+<style></style>
---
+++ my-vue-app/src/components/AppHero.vue
@@ -0,0 +1,17 @@
+<script>
+export default {
+  data() {
+    return {};
+  },
+};
+</script>
+
+<template>
+  <img src="../assets/img/download.jpeg" alt="" />
+</template>
+
+<style>
+img {
+  width: 400px;
+}
+</style>
